feb1ab93d17fb235e0f93ce2bcd4ac5326e66753
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-01-29 08:32:21 -0300

    Recuperando estado da autenticação

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,11 @@
-import axios from 'axios'
-
+import axios from "axios";
+import { parseCookies } from "nookies";
+const cookies = parseCookies();
 export const api = axios.create({
-    baseURL: 'http://localhost:3333',
-    // 1. clone, 2.yarn, 3.yarn dev: https://github.com/rocketseat-education/ignite-reactjs-auth-backend
+  baseURL: "http://localhost:3333",
+  // 1. clone, 2.yarn, 3.yarn dev: https://github.com/rocketseat-education/ignite-reactjs-auth-backend
+
+  headers: {
+    Authorization: `Bearer ${cookies["nextauth.token"]}`,
+  },
 });
